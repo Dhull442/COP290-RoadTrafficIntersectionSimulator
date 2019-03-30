9967c3871c8c966ec0201a56db2323fe430ad00e
Ananye Agarwal <ananayagarwal@gmail.com>
2019-03-30 21:40:50 +0530

    Made some Road methods private

--- Road.h
+++ Road.h
@@ -13,18 +13,21 @@ class Road {
         std::string signal; // The signal value at this time
         std::vector<std::pair<double, double> > map;
         std::vector<double> calculateBackEnds();
+        void updateUnrestrictedpositions(double delT);
+        void addtoLanes(Vehicle* vehicle,int numlanesreq,int toplane);
+        double findLast(std::vector<Vehicle*> vehicles);
+        void updateLane(int a,Vehicle* b);
+        void removeFromLane(int lane,Vehicle* v);
+        bool hasSpace(std::vector<Vehicle*> Vehicles,double front,double back);
     public:
         // default vehicle Parameters
-        #ifdef RENDER_ENGINE_H
         RenderEngine engine;
-        #endif
         double default_maxspeed = 1;
         double default_acceleration = 1;
         double default_length = 2;
         double default_width = 2;
         double default_safety_distance = 1;
         int default_skill = 1;
-
         double length;
         double width;
         double signalPosition;
@@ -33,17 +36,17 @@ class Road {
         std::vector< int > signal_rgb;
         // Pointer to the Vehicle objects on the road
         std::vector<Vehicle*> vehicles;
-        std::vector< std::vector<Vehicle*> > laneVehicles; // a pair of vehicles in lane and it's back end;
+        // Pointer to vehicles in Lanes
+        std::vector< std::vector<Vehicle*> > laneVehicles;
         // Initialize the Road object
         Road(int id, double length, double width);
         Road(int id);
         Road();
+        // Update the simulation in a step of delT
         void updateSim(double delT);
         void setDefaults(double maxspeed, double acceleration,double length, double width,int skill, double sdistance);
         // Add a Vehicle to the road
         void addVehicle(Vehicle* vehicle,std::string color);
-        void updateUnrestrictedpositions(double delT);
-        void addtoLanes(Vehicle* vehicle,int numlanesreq,int toplane);
         // First vehicle obstacle in a lane
         // double firstObstacle(double startPos,double length, double topRow, double botRow );
         double firstObstacle(Vehicle* vehicle);
@@ -51,16 +54,11 @@ class Road {
         void initLanes(int lanes);
         std::pair<double,double> initPosition(Vehicle* vehicle);
         void error_callback(std::string errormsg);
+        void changeLane(Vehicle* vehicle);
         // Run the simulation on the road for time t
         void runSim(double t);
         void setSignal(std::string signal);
-        double findLast(std::vector<Vehicle*> vehicles);
-        void changeLane(Vehicle* vehicle);
-        void updateLane(int a,Vehicle* b);
-        void removeFromLane(int lane,Vehicle* v);
         void printLanes();
-        bool hasSpace(std::vector<Vehicle*> Vehicles,double front,double back);
-        // std::pair<double,double> initPosition(Vehicle* v);
 
     };
 
